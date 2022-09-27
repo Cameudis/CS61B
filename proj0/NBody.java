e29c9692c0b4c081a474d3d99f76ad7f1dde94b2
Cameudis <cameudis@gmail.com>
2022-09-27 18:45:18 +0800

    completed NBody.readPlanet()

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -6,4 +6,24 @@ class NBody {
 
         return radius;
     }
+
+    public static Planet[] readPlanets(String file_name) {
+        In in_file = new In(file_name);
+        int n = in_file.readInt();
+        in_file.readDouble();   // throw radius
+
+        Planet[] planets = new Planet[n];
+        for (int i = 0; i < n; i++) {
+            double xpos, ypos, xvel, yvel, m;
+            xpos = in_file.readDouble();
+            ypos = in_file.readDouble();
+            xvel = in_file.readDouble();
+            yvel = in_file.readDouble();
+            m    = in_file.readDouble();
+            String pname = in_file.readString();
+            planets[i] = new Planet(xpos, ypos, xvel, yvel, m, pname);
+        }
+
+        return planets;
+    }
 }
